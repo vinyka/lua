c3054b080dd8a1b05213504fd917fc6c2bd0d6f5
vinyka <wrox591@gmail.com>
2017-09-19 20:09:09 -0400

    Add files via upload

---
+++ Calc/main.lua
@@ -0,0 +1,13 @@
+require ("teste")
+require ("teste1")
+print("####################################")
+print("#			Welcome				  #")
+print("#			1 : + ou -   		  #")
+print("#			2 : / ou %			  #")
+print("####################################")
+n = io.read(*number)
+if n == 1 then
+	function(teste)
+else
+	function(teste1)
+end
---
+++ Calc/teste.lua
@@ -0,0 +1,32 @@
+require ("main")
+function teste(x, y)
+	m=("number")
+	z=("number")
+	print("Digite Os valores: ")
+	print("Após isto digite o operador 1 : + 2: -")
+	n = io.read(*umber)
+	n = io.read(m)
+	if m == 1 then
+		z = (x + y)
+		print("Seu Resultado e : ")
+		print(z)
+		print("Muito facil, deseja fazer outra? 1 : sim 2 : nao ")
+		n = io.read(*number)
+		if n == 1 then
+			function(teste)
+		else
+			function(main)
+		end
+	else
+		z = (x - y)
+		print("Seu Resultado e : ")
+		print(z)
+		print("Muito facil, deseja fazer outra? 1 : sim 2 : nao")
+		n = io.read(*number)
+		if n == 1 then
+			function(teste)
+		else
+			function(main)
+		end
+	end
+end
